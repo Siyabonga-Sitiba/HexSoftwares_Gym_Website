d727c0d0c670d60b9ec629393e1b827a9c480f0e
Siyabonga-Sitiba <siyabongmelissa@gmail.com>
2025-01-07 14:04:31 +0200

    Finished commit changes
    gym website - HTML - CSS - JavaScript

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//animation text
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Physical Fitness', 'Weight Gain', 'Fat Loss', 'Weight Lifting', 'Running'],
+    typeSpeed: 50,
+    backspeed: 60,
+    backdelay: 1000,
+    loop: true,
+  });
+
+
+  // page animation 
+
+AOS.init({
+    offset: 300,
+    duration: 1400
+  });
+  
+
